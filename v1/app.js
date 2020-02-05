258464047875c9c87aa012fd910b0798e27bcb42
Shivam Mehta <shivam.mehta2019@vitstudent.ac.in>
2020-02-05 23:15:08 +0530

    styling forms

--- v1/app.js
+++ v1/app.js
@@ -3,12 +3,14 @@ var app=express();
 var bodyParser=require('body-parser');
 app.use(bodyParser.urlencoded({extended:true}));
 app.set("view engine","ejs");
+
+app.use(express.static("public"))
 app.get("/",function(req,res){
     res.render("landing");
 });
 var campgrounds= [
-    {name:"Salmon Creek",image:"https://images.unsplash.com/photo-1537225228614-56cc3556d7ed?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1950&q=80"},
-    {name:"Granite Hill",image:"https://images.unsplash.com/photo-1537905569824-f89f14cceb68?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=600&q=60"},
+    {name:"Salmon Creek",image:"https://images.unsplash.com/photo-1571069756236-9d9322054086?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=600&q=60"},
+    {name:"Granite Hill",image:"https://q-xx.bstatic.com/xdata/images/hotel/840x460/178852959.jpg?k=e968b270d8259ebbdedad907a5fedb57134ce3a111e276f9dcfa8aaf3dc987c5&o="},
     {name:"Mountain Goat's rest",image:"https://images.unsplash.com/photo-1508873696983-2dfd5898f08b?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=600&q=60"},
 ]
 app.get("/campgrounds",function(req,res){
@@ -17,15 +19,16 @@ app.get("/campgrounds",function(req,res){
 
 });
 app.get("/campgrounds/new",function(req,res){
+    res.render("new");
+    
+
+});
+app.post("/campgrounds",function(req,res){
     var name=req.body.name;
     var image=req.body.image;
     var newCampground={name:name,image:image};
     campgrounds.push(newCampground);
     res.redirect("/campgrounds");
-
-});
-app.post("/campgrounds",function(req,res){
-res.send("lol");
 })
 app.listen(3000,function()
 {
